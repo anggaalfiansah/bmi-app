e1f214b06d159ecf3bfe49c4104212b63d2934e0
Angga Alfiansah <anggaalfiansah26@gmail.com>
2025-02-09 11:37:07 +0700

    build(scaleup-fe) : Penambahan vite-plugin-chunk-split & vite-plugin-image-optimizer pada konfigurasi vite

--- scaleup-fe/vite.config.ts
+++ scaleup-fe/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    chunkSplitPlugin({
+      strategy: "unbundle",
+      customSplitting: {
+        vendor: [/node_modules/],
+        assets: [/src\/assets/],
+        components: [/src\/components/],
+        app: [/src/],
+      },
+    }),
+    ViteImageOptimizer(),
+  ],
+});
